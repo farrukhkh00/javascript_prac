a8ae8369d3fa3cbc07280a279916fa15326c1bf7
Fahad Khan <diary1234590@gmail.com>
2024-02-28 12:54:40 +0500

    chap5

---
+++ chapter5/app.js
@@ -0,0 +1,87 @@
+// q1
+var x = 25;
+var y = 7;
+alert(x % y);
+
+//q2
+
+var a = 15;
+var b = 4;
+alert((a * b) + a + 10 + (b - 2));
+
+//q3
+
+var c = 12;
+var d = 3;
+alert(c ** d);
+
+//q4
+
+var e = 10;
+var f = 3;
+alert(e += f * 2);
+
+// q5
+
+var g = 20;
+var h = 4;
+alert(g / h + (5 + g));
+
+//q6
+
+var i = 8;
+var j = 3;
+alert(i - 2 * j);
+
+// q7
+
+var k = 18;
+var l = 5;
+alert(k - l + 2);
+
+// q8
+
+var m = 7;
+var n = 2;
+alert(m ** n);
+
+//q 9
+
+var o = 13;
+var p = 4;
+alert(o % (p + 2));
+
+// q10
+
+var q = 16;
+var r = 3;
+alert(q / 2 * r);
+
+// q11
+
+var s = 25;
+var t = 3;
+alert((s - 1) / t);
+
+// q12
+
+var u = 11;
+var v = 3;
+alert(u - v + 2);
+
+// q13
+var w = 22;
+var z = 5;
+alert(w + (z ** 2));
+
+// q14
+
+var xy = 19;
+var xz = 4;
+alert(xy % (xz + 1));
+
+var ac = 10;
+var bc = 3;
+alert(ac * (bc - 2));
+
+
