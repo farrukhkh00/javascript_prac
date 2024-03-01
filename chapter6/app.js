ea349a51624fba80b4bcf366ead50479fbd271bf
Fahad Khan <diary1234590@gmail.com>
2024-03-01 12:30:00 +0500

    chap 6,7 done

---
+++ chapter6/app.js
@@ -0,0 +1,60 @@
+
+document.write("Open console to see the results");
+// q1
+var count = 8;
+count++;
+console.log(count);
+
+//q2
+
+var x = 15;
+--x;
+console.log(x);
+
+// q3
+
+var num = 25;
+num--;
+console.log(num);
+
+// q4
+
+var value = 10;
+++value;
+console.log(value);
+
+// q5
+var index = 5;
+index--;
+console.log(index);
+
+// q6
+
+var quantity = 12;
+--quantity;
+console.log(quantity);
+
+// q7
+
+var counter = 18;
+counter++;
+console.log(counter);
+
+// q8
+
+var score = 30;
+score--;
+console.log(score);
+
+// q9
+
+var value = 20;
+++value;
+console.log(value);
+
+// q10
+
+var countrr = 8;
+countrr--;
+console.log(countrr);
+
---
+++ chapter7/app.js
@@ -0,0 +1,44 @@
+// q1
+
+var firstName = "John " + "Doe";
+alert(firstName);
+
+//q2
+
+var city = "New " + "York"
+alert(city);
+
+//q3
+ var fruit = "apple" + " pie";
+ alert(fruit);
+
+ // q4
+
+ var animal = "black " + "Bird";
+ alert(animal);
+
+ // q5
+
+ alert("Javascript " + "is fun");
+
+ // q6
+
+ alert("Happy" +  " Birthday " + "To you" );
+
+ //q7
+
+ alert( "Open Source "+ "Intelligence");
+
+ //q8 
+
+ alert("Goood Luck" + " and Have fun");
+ 
+
+ // q9
+
+ alert("Web " + "Design " + "Development");
+
+ // q10
+ 
+ alert("Artifical " + "Neural " + "Networks");
+ 
